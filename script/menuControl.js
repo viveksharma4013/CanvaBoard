d5aa3f2d4b591e641b020cb76079803617d66a1f
Vivek Sharma <25190@TELXSIDS.NET>
2021-11-29 20:10:44 +0530

    sticky notes added and pen and eraser ui added

---
+++ script/menuControl.js
@@ -0,0 +1,66 @@
+// Elements
+var menu=document.getElementsByClassName("menu");
+var tools=document.getElementById("tools");
+var pencil_icon=document.getElementById("pencil-icon");
+var eraser_icon=document.getElementById("eraser-icon");
+var sticy_note_icon=document.getElementById("sticy_note-icon");
+var upload_icon=document.getElementById("upload-icon");
+var color_selector=document.getElementById("color-container");
+var pencil_width_setter=document.getElementsByClassName("pencil-container__width")[0]
+var eraser_width_setter=document.getElementsByClassName("eraser-container__width")[0]
+
+// varibales
+var menuClosed=true;
+var pencil_select_closed=true;
+var eraser_select_closed=true;
+// open tools with menu button
+menu[0].onclick=function(e){
+    if(menuClosed){
+        tools.className="tools";
+    }else{
+        tools.className="hidden";
+    }
+    menuClosed=!menuClosed;
+}
+pencil_icon.onclick=function(e){
+    if(pencil_select_closed){
+        color_selector.className="color-container";
+        pencil_width_setter.classList.remove("hidden")
+    }else{
+        color_selector.className="hidden";
+        pencil_width_setter.classList.add("hidden")
+    }
+    pencil_select_closed=!pencil_select_closed;
+}
+eraser_icon.onclick=function(e){
+    if(eraser_select_closed){
+        eraser_width_setter.classList.remove("hidden")
+    }else{
+        eraser_width_setter.classList.add("hidden")
+    }
+    eraser_select_closed=!eraser_select_closed;
+}
+// change width of pencil using this function
+function pencilWidthChange(e){
+    console.log(e)
+}
+function eraserWidthChange(e){
+    console.log("eraser",e)
+}
+
+// create new stickynote
+sticy_note_icon.addEventListener('click',()=>{
+    createSticky();
+});
+upload_icon.addEventListener('click',()=>{
+    let el=document.createElement('input');
+    el.setAttribute('type','file');
+    el.click();
+    el.addEventListener('change',()=>{
+        let file=el.files[0];
+        let url=URL.createObjectURL(file);
+        createStickyWithImage(url);
+    })
+});
+
+      
---
+++ script/stickyNoteActions.js
@@ -0,0 +1,91 @@
+// drap n drop
+
+function dragNDrop(element,event){
+
+    let shiftX = event.clientX - element.getBoundingClientRect().left;
+    let shiftY = event.clientY - element.getBoundingClientRect().top;
+  
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
+    document.body.append(element);
+  
+    moveAt(event.pageX, event.pageY);
+  
+    function moveAt(pageX, pageY) {
+      element.style.left = pageX - shiftX + 'px';
+      element.style.top = pageY - shiftY + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // move the element on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // drop the element, remove unneeded handlers
+    element.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      element.onmouseup = null;
+    };
+};
+
+function createSticky(){
+  let sticky_container=document.createElement('div');
+    sticky_container.setAttribute('class','sticky_container');
+    sticky_container.innerHTML=`
+    <div class="sticky_container__head">
+        <div class="sticky_container__head__minimize"></div>
+        <div class="sticky_container__head__close"></div>
+    </div>
+    <textarea class="sticky_container__text"></textarea>
+    `;
+    document.body.appendChild(sticky_container);
+
+    let minimize=sticky_container.querySelectorAll('.sticky_container__head__minimize')[0];
+    let close=sticky_container.querySelectorAll('.sticky_container__head__close')[0]
+    stickyNoteAction(minimize,close,sticky_container);
+    sticky_container.onmousedown = function(event) {
+        dragNDrop(sticky_container,event);
+    }
+    sticky_container.ondragstart = function() {
+        return false;
+    }
+}
+function createStickyWithImage(imgUrl){
+  let sticky_container=document.createElement('div');
+    sticky_container.setAttribute('class','sticky_container');
+    sticky_container.innerHTML=`
+    <div class="sticky_container__head">
+        <div class="sticky_container__head__minimize"></div>
+        <div class="sticky_container__head__close"></div>
+    </div>
+      <img src="${imgUrl}"/>
+    `;
+    document.body.appendChild(sticky_container);
+
+    let minimize=sticky_container.querySelectorAll('.sticky_container__head__minimize')[0];
+    let close=sticky_container.querySelectorAll('.sticky_container__head__close')[0]
+    stickyNoteAction(minimize,close,sticky_container);
+    sticky_container.onmousedown = function(event) {
+        dragNDrop(sticky_container,event);
+    }
+    sticky_container.ondragstart = function() {
+        return false;
+    }
+}
+
+
+
+function stickyNoteAction(minimize,close,element){
+    close.addEventListener('click',(e)=>{
+        console.log(element)
+        element.remove();
+    })
+    minimize.addEventListener('click',()=>{
+        let textArea=element.querySelector('.sticky_container__text');
+        let displayProperty=getComputedStyle(textArea).getPropertyValue('display');
+        if(displayProperty=="none") textArea.style.display="block";
+        else textArea.style.display="none";
+    })
+}
